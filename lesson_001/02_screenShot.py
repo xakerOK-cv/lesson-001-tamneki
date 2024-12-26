d57868d8aabcda89f816133afa9f912102955433
DELL E5480 <tamneki8841@gmail.com>
2024-12-26 14:44:29 +0200

    Сделано!

--- lesson_001/02_screenShot.py
+++ lesson_001/02_screenShot.py
@@ -3,4 +3,4 @@
 # https://app.prntscr.com/ru/
 # У PyCharm має бути відкритий файл 01_iosInfoPyVer.py. Внизу має бути видно результат виконання цього файлу. 
 
-# TODO тут розмістіть посилання на скриншот.
+# Сделано https://ru.paste.pics/bc4ef9a952dab801e3e23c1cd6e9b7e5
